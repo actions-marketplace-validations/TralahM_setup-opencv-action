ee398d498295d2b7a20a926d1caf75744f61d8a9
TralahM <briantralah@gmail.com>
2023-08-03 03:56:47 +0300

    fix python-dev not found, add debian.net jessie

--- index.js
+++ index.js
@@ -6,34 +6,41 @@ async function run() {
     try {
         const requestedVersion = core.getInput('opencv-version') || '4.0.0';
         const useMasterBranch = requestedVersion == 'master' ||
-                                requestedVersion == 'dev'    ||
-                                requestedVersion == 'latest' ||
-                                requestedVersion == 'last';
+            requestedVersion == 'dev' || requestedVersion == 'latest' ||
+            requestedVersion == 'last';
 
         const version = useMasterBranch ? 'master' : requestedVersion;
         const extraModules = core.getInput('opencv-extra-modules') == 'true';
-        const installDeps = core.getInput('install-deps') == undefined || core.getInput('install-deps') == 'true';
+        const installDeps = core.getInput('install-deps') == undefined ||
+            core.getInput('install-deps') == 'true';
 
-        const CMAKE_CXX_COMPILER         = core.getInput('CMAKE_CXX_COMPILER');
-        const CMAKE_INSTALL_PREFIX       = core.getInput('CMAKE_INSTALL_PREFIX');
-        const WITH_TBB                   = core.getInput('WITH_TBB');
-        const WITH_IPP                   = core.getInput('WITH_IPP');
-        const BUILD_NEW_PYTHON_SUPPORT   = core.getInput('BUILD_NEW_PYTHON_SUPPORT');
-        const WITH_V4L                   = core.getInput('WITH_V4L');
-        const ENABLE_PRECOMPILED_HEADERS = core.getInput('ENABLE_PRECOMPILED_HEADERS');
-        const INSTALL_C_EXAMPLES         = core.getInput('INSTALL_C_EXAMPLES');
-        const INSTALL_PYTHON_EXAMPLES    = core.getInput('INSTALL_PYTHON_EXAMPLES');
-        const BUILD_EXAMPLES             = core.getInput('BUILD_EXAMPLES');
-        const WITH_QT                    = core.getInput('WITH_QT');
-        const WITH_OPENGL                = core.getInput('WITH_OPENGL');
-        const GENERATE_PKGCONFIG         = core.getInput('GENERATE_PKGCONFIG');
+        const CMAKE_CXX_COMPILER = core.getInput('CMAKE_CXX_COMPILER');
+        const CMAKE_INSTALL_PREFIX = core.getInput('CMAKE_INSTALL_PREFIX');
+        const WITH_TBB = core.getInput('WITH_TBB');
+        const WITH_IPP = core.getInput('WITH_IPP');
+        const BUILD_NEW_PYTHON_SUPPORT =
+            core.getInput('BUILD_NEW_PYTHON_SUPPORT');
+        const WITH_V4L = core.getInput('WITH_V4L');
+        const ENABLE_PRECOMPILED_HEADERS =
+            core.getInput('ENABLE_PRECOMPILED_HEADERS');
+        const INSTALL_C_EXAMPLES = core.getInput('INSTALL_C_EXAMPLES');
+        const INSTALL_PYTHON_EXAMPLES =
+            core.getInput('INSTALL_PYTHON_EXAMPLES');
+        const BUILD_EXAMPLES = core.getInput('BUILD_EXAMPLES');
+        const WITH_QT = core.getInput('WITH_QT');
+        const WITH_OPENGL = core.getInput('WITH_OPENGL');
+        const GENERATE_PKGCONFIG = core.getInput('GENERATE_PKGCONFIG');
 
         if (installDeps) {
             core.startGroup('Install dependencies');
-            await exec.exec('sudo add-apt-repository "deb http://security.ubuntu.com/ubuntu xenial-security main"');
+            await exec.exec(
+                'sudo add-apt-repository "deb http://security.ubuntu.com/ubuntu xenial-security main"');
+            await exec.exec(
+                'sudo add-apt-repository "deb http://debian.net/debian jessie main"');
             await exec.exec('sudo apt-get update');
             await exec.exec('sudo apt-get remove x264 libx264-dev -y');
-            await exec.exec('sudo apt-get install -y ' +
+            await exec.exec(
+                'sudo apt-get install -y ' +
                 'build-essential checkinstall git cmake pkg-config yasm ' +
                 'git gfortran libjpeg8-dev libjasper1 libjasper-dev libpng-dev ' +
                 'libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev ' +
@@ -45,35 +52,38 @@ async function run() {
                 'libvorbis-dev libxvidcore-dev ' +
                 'libopencore-amrnb-dev libopencore-amrwb-dev ' +
                 'libtbb2 libtiff-dev ' +
-                'python-dev python-numpy ' +
-                'x264 v4l-utils '
-            );
+                'python-dev-is-python3 python-numpy ' +
+                'x264 v4l-utils ');
             core.endGroup();
         }
 
         core.startGroup('Download source code');
-        await exec.exec(`git clone https://github.com/opencv/opencv.git --branch ${version} --depth 1`);
+        await exec.exec(
+            `git clone https://github.com/opencv/opencv.git --branch ${
+                version} --depth 1`);
 
-        if(extraModules) {
-            await exec.exec(`git clone https://github.com/opencv/opencv_contrib.git --branch ${version} --depth 1`);
+        if (extraModules) {
+            await exec.exec(
+                `git clone https://github.com/opencv/opencv_contrib.git --branch ${
+                    version} --depth 1`);
         }
         core.endGroup();
-      
+
         const cmakeCmd = 'cmake -S opencv -B opencv/build ' +
-            ' -D CMAKE_CXX_COMPILER=' + CMAKE_CXX_COMPILER + 
+            ' -D CMAKE_CXX_COMPILER=' + CMAKE_CXX_COMPILER +
             ' -D CMAKE_INSTALL_PREFIX=' + CMAKE_INSTALL_PREFIX +
-            ' -D WITH_TBB=' + WITH_TBB + 
-            ' -D WITH_IPP=' + WITH_IPP + 
+            ' -D WITH_TBB=' + WITH_TBB + ' -D WITH_IPP=' + WITH_IPP +
             ' -D BUILD_NEW_PYTHON_SUPPORT=' + BUILD_NEW_PYTHON_SUPPORT +
             ' -D WITH_V4L=' + WITH_V4L +
             ' -D ENABLE_PRECOMPILED_HEADERS=' + ENABLE_PRECOMPILED_HEADERS +
             ' -D INSTALL_C_EXAMPLES=' + INSTALL_C_EXAMPLES +
             ' -D INSTALL_PYTHON_EXAMPLES=' + INSTALL_PYTHON_EXAMPLES +
-            ' -D BUILD_EXAMPLES=' + BUILD_EXAMPLES +
-            ' -D WITH_QT=' + WITH_QT +
+            ' -D BUILD_EXAMPLES=' + BUILD_EXAMPLES + ' -D WITH_QT=' + WITH_QT +
             ' -D WITH_OPENGL=' + WITH_OPENGL +
             ' -D OPENCV_GENERATE_PKGCONFIG=' + GENERATE_PKGCONFIG +
-            (extraModules ? ' -D OPENCV_EXTRA_MODULES_PATH=./opencv_contrib/modules ' : '');
+            (extraModules ?
+                 ' -D OPENCV_EXTRA_MODULES_PATH=./opencv_contrib/modules ' :
+                 '');
 
         console.log(`Compile cmd: ${cmakeCmd}`);
 
@@ -83,16 +93,16 @@ async function run() {
         await exec.exec('sudo make -C opencv/build install');
         await exec.exec('sudo ldconfig');
         core.endGroup();
-        
+
         // Clean up?
         core.startGroup('Cleanup');
         await exec.exec('rm -rf opencv');
         await exec.exec('rm -rf opencv_contrib');
         core.endGroup();
 
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
 
 run();
